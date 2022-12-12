8b24ef272787e5e903b02b25552aa305cbfb1a36
FathanAbi <90834092+FathanAbi@users.noreply.github.com>
2022-12-12 20:37:07 +0700

    Update Main.java
    Tambahkan menu screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,24 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage Stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+			Scene menuScene = new Scene(root);
+			Stage.setScene(menuScene);
+			
+			Stage.setTitle("Connect 4");
+			Stage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
